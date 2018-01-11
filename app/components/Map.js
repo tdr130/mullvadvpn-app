15a6511112ad145b920b8fab955ae179bb1dbb2e
Andrej Mihajlov <and@mullvad.net>
2018-01-11 14:42:25 +0100

    Wrap country marker for conditional rendering

--- app/components/Map.js
+++ app/components/Map.js
@@ -1,7 +1,6 @@
 // @flow
 
 import React, { Component } from 'react';
-import { Motion, spring } from 'react-motion';
 import { ComposableMap, ZoomableGroup, Geographies, Geography, Markers, Marker } from 'react-simple-maps';
 
 import countriesJSON from '../assets/geo/countries.geo.json';
@@ -21,23 +20,6 @@ export type MapProps = {
 
 export default class Map extends Component {
   props: MapProps
-
-  state: {
-    initial: {
-      zoom: number,
-      location: Coordinate2d
-    }
-  }
-
-  constructor(props: MapProps) {
-    super(props);
-    this.state = {
-      initial: {
-        zoom: props.zoom,
-        location: props.location
-      }
-    };
-  }
 
   render() {
     const projectionConfig = {
@@ -67,18 +49,6 @@ export default class Map extends Component {
 
     const markerLocation = swapLatLon(this.props.location);
     const cameraLocation = swapLatLon(this.props.location);
-    const initialLocation = swapLatLon(this.state.initial.location);
-    const initialStyle = {
-      zoom: this.state.initial.zoom,
-      x: initialLocation[0],
-      y: initialLocation[1]
-    };
-
-    const motionStyle = {
-      zoom: spring(this.props.zoom, {stiffness: 210, damping: 20}),
-      x: spring(cameraLocation[0], {stiffness: 210, damping: 20}),
-      y: spring(cameraLocation[1], {stiffness: 210, damping: 20}),
-    };
 
     const userMarker = (
       <Marker key={ 'pin-marker' } marker={{ coordinates: markerLocation }}>
@@ -86,44 +56,54 @@ export default class Map extends Component {
       </Marker>
     );
 
-    const countryMarkers = countriesJSON.features.map((item, i) => (
-      <Marker key={ `country-marker-${i}` } marker={{ coordinates: item.geometry.coordinates }}>
-        <text fill="rgba(255,255,255,.4)" fontSize="22" textAnchor="middle">
+    const IS_MARKER_IN_VISIBLE_BOUNDARIES = () => {
+      return false;
+    };
+
+    const CountryMarker = ({ item, projection, ...otherProps }) => {
+      const translation = projection()(item.geometry.coordinates);
+
+      return !IS_MARKER_IN_VISIBLE_BOUNDARIES(translation) ? null : (
+        <Marker marker={{ coordinates: item.geometry.coordinates }}
+          projection={ projection }
+          { ...otherProps }>
+          <text fill="rgba(255,255,255,.4)" fontSize="22" textAnchor="middle">
+            { item.properties.name }
+          </text>
+        </Marker>
+      );
+    };
+
+    const countryMarkers = countriesJSON.features.map(item => (
+      <CountryMarker key={ `a${item.id}` } item={ item } />
+    ));
+
+    const cityMarkers = [] || citiesJSON.features.map((item) => (
+      <Marker key={ `b${item.id}` } marker={{ coordinates: item.geometry.coordinates }}>
+        <circle r="2" fill="rgba(255,255,255,.8)" />
+        <text x="0" y="-10" fill="rgba(255,255,255,.8)" fontSize="14" textAnchor="middle">
           { item.properties.name }
         </text>
       </Marker>
     ));
 
-    const cityMarkers = citiesJSON.features.map((item, i) => (
-      <Marker key={ `city-marker-${i}` } marker={{ coordinates: item.geometry.coordinates }}>
-        <circle r="2" fill="rgba(255,255,255,.8)" />
-        <text x="0" y="-10" fill="rgba(255,255,255,.8)" fontSize="14" textAnchor="middle">
-          { item.properties.NAME }
-        </text>
-      </Marker>
-    ));
-
     return (
-      <Motion defaultStyle={ initialStyle } style={ motionStyle }>
-        {({zoom, x, y}) => (
-          <ComposableMap width={ 800 } height={ 450 } style={ mapStyle } projectionConfig={ projectionConfig }>
-            <ZoomableGroup center={ [x, y] } zoom={ zoom } disablePanning={ false }>
-              <Geographies geography={ './assets/geo/geometry.json' }>
-                {(geographies, projection) => geographies.map((geography, i) => (
-                  <Geography
-                    key={ `geography-${i}` }
-                    geography={ geography }
-                    projection={ projection }
-                    style={ geographyStyle } />
-                ))}
-              </Geographies>
-              <Markers>
-                { countryMarkers.concat(cityMarkers, [userMarker]) }
-              </Markers>
-            </ZoomableGroup>
-          </ComposableMap>
-        )}
-      </Motion>
+      <ComposableMap width={ 800 } height={ 450 } style={ mapStyle } projectionConfig={ projectionConfig }>
+        <ZoomableGroup center={ cameraLocation } zoom={ this.props.zoom } disablePanning={ false }>
+          <Geographies geography={ './assets/geo/geometry.json' }>
+            {(geographies, projection) => geographies.map((geography, i) => (
+              <Geography
+                key={ `geography-${i}` }
+                geography={ geography }
+                projection={ projection }
+                style={ geographyStyle } />
+            ))}
+          </Geographies>
+          <Markers>
+            { countryMarkers.concat(cityMarkers, [userMarker]) }
+          </Markers>
+        </ZoomableGroup>
+      </ComposableMap>
     );
   }
 }
