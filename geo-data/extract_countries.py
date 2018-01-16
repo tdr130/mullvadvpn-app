0f431d7f28ea3699e58c812ebc85da3da796688d
Andrej Mihajlov <and@mullvad.net>
2018-01-16 21:18:51 +0100

    Use representative_point instead of centroid which can be outside of the geometry as in case with France.

--- geo-data/extract_countries.py
+++ geo-data/extract_countries.py
@@ -1,5 +1,6 @@
-import os, json, fiona
+# import order is important, see https://github.com/Toblerity/Shapely/issues/553
 from shapely.geometry import shape, mapping
+import os, json, fiona
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 dataset_name = "ne_50m_admin_0_countries"
@@ -14,7 +15,7 @@ with fiona.open(input_path) as source:
         geometry = feat["geometry"]
 
         # convert country polygon to centroid
-        geometry.update(mapping(shape(geometry).centroid))
+        geometry.update(mapping(shape(geometry).representative_point()))
 
         # lowercase all keys
         props = dict((k.lower(), v) for k, v in feat["properties"].iteritems())
